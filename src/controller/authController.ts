2f4e5ab482fc349c2822d23c8e44baf54505ec9d
talent-oleksii <oleksiikaravanov@gmail.com>
2023-11-05 16:02:31 +0200

    add signin and signup

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,18 +2,45 @@ import { RequestHandler, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes/build/cjs/status-codes";
 import axios from 'axios';
 
+import useAirTable from "../util/useAirTable";
 import log from "../util/logger";
 
 const signIn: RequestHandler = async (req: Request, res: Response) => {
     log.info("Sign in api called");
+    const { email, password } = req.query;
 
-    return res.status(StatusCodes.OK).json("auth");
+    console.log('d:', email, password);
+    useAirTable('Users', 'get', {
+        'Email': email,
+        'Password': password,
+    })?.then(data => {
+        console.log('dat:', data.data);
+        return res.status(StatusCodes.OK).json(data.data);
+    }).catch(error => {
+        console.log('err:', error.message);
+        return res.status(StatusCodes.OK).json({ message: error.message });
+    })
+
 };
 
 const clientSignUp: RequestHandler = async (req: Request, res: Response) => {
     log.info('Sign up api called');
 
-    return res.status(StatusCodes.OK).json();
+    const { fullName, email, password, company } = req.body;
+
+    useAirTable("Users", 'post', {
+        'Full Name': fullName,
+        'Email': email,
+        'Password': password,
+        'Company Name': company,
+        'User Group': 'Client',
+    })?.then(data => {
+        console.log('dat:', data);
+        return res.status(StatusCodes.OK).json(data.data);
+    }).catch(error => {
+        console.log('err:', error.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    });
 };
 
 const auth = {
---
+++ src/util/useAirTable.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const useAirTable = (tableName: string, methodType: string, data: any) => {
+    const apiUrl = 'https://api.airtable.com/v0/';
+    const airtablePat = process.env.AIRTABLE_PAT;
+    const baseId = process.env.AIRTABLE_BASE_ID;
+
+    if (methodType === 'get') {
+        let query = "AND(";
+        const keyLength = Object.keys(data).length;
+        Object.keys(data).forEach((key, index) => {
+            query = `${query}{${key}} = '${data[key]}'`;
+            if (index < keyLength - 1)
+            query = `${query}, `;
+        });
+        query = query.concat(')');
+
+        console.log('query:', query);
+        return axios.get(`${apiUrl}${baseId}/${tableName}`, {
+            headers: {
+                'Authorization': `Bearer ${airtablePat}`,
+            },
+            params: {
+                filterByFormula: query,
+            }
+        });
+    } else if (methodType === 'post') {
+        return axios.post(`${apiUrl}${baseId}/${tableName}`, {
+            fields: data,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${airtablePat}`,
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+};
+
+export default useAirTable;
