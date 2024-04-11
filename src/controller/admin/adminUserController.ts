27c5ad56cbf2c660972d66db8430102ab8eecda7
Sahil Gupta <sahilhgupta562@gmail.com>
2024-04-12 04:51:09 +0530

    Worked on my team api

--- src/controller/admin/adminUserController.ts
+++ src/controller/admin/adminUserController.ts
@@ -1,96 +1,184 @@
-import { RequestHandler, Request, Response } from 'express';
+import { RequestHandler, Request, Response } from "express";
 
-import db from '../../util/db';
-import { StatusCodes } from 'http-status-codes';
+import db from "../../util/db";
+import { StatusCodes } from "http-status-codes";
 
-const getAccountManagers: RequestHandler = async (_req: Request, res: Response) => {
+const getAccountManagers: RequestHandler = async (
+  _req: Request,
+  res: Response
+) => {
   try {
-    const assigners = await db.query('SELECT * from admin_user WHERE role = $1', ['account_manager']);
+    const assigners = await db.query(
+            `SELECT * FROM (SELECT 
+          admin_user.id,
+          admin_user.create_time, 
+          admin_user.name, 
+            admin_user.email, 
+            COALESCE(SUM(spent), 0) AS spent, 
+            COALESCE(SUM(billed), 0) AS billed, 
+            COUNT(campaign.id) AS campaign_count, 
+            COUNT(DISTINCT user_list.id) AS client_count,
+            SUM(CASE WHEN campaign.state = 'active' AND campaign.complete_date is null THEN 1 ELSE 0 END) AS active_count,
+          SUM(CASE WHEN campaign.state = 'active' AND campaign.complete_date is not null THEN 1 ELSE 0 END) AS completed_count,
+            SUM(CASE WHEN campaign.state = 'draft' THEN 1 ELSE 0 END) AS draft_count,
+            SUM(CASE WHEN campaign.state IN ('active', 'completed') THEN campaign.price ELSE 0 END) AS total_budget 
+        FROM 
+            user_list 
+        LEFT JOIN 
+            campaign ON user_list.email = campaign.email 
+        LEFT JOIN 
+            admin_user ON user_list.id::TEXT = ANY(STRING_TO_ARRAY(admin_user.assigned_users, ','))
+        WHERE admin_user.role = $1 and admin_user.assigned_users is not null
+        GROUP BY 
+            admin_user.id
+          
+        Union
+        
+        SELECT 
+          admin_user.id,
+          admin_user.create_time, 
+          admin_user.name, 
+            admin_user.email, 
+          0 AS spent, 
+            0 AS billed, 
+            0 AS campaign_count, 
+          0 AS client_count,
+            0 AS active_count,
+          0 AS completed_count,
+            0 AS draft_count,
+            0 AS total_budget 
+        FROM admin_user WHERE admin_user.role = $1 and admin_user.assigned_users is null) AS combined_results
+        ORDER BY 
+            combined_results.name;`,
+      ["account_manager"]
+    );
 
     return res.status(StatusCodes.OK).json(assigners.rows);
   } catch (error: any) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
   }
 };
 
-const assignAccountManager: RequestHandler = async (req: Request, res: Response) => {
+const assignAccountManager: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const { userId, manager } = req.body;
 
-    const origin = (await db.query('SELECT assigned_users from admin_user where id = $1', [manager])).rows[0].assigned_users;
+    const origin = (
+      await db.query("SELECT assigned_users from admin_user where id = $1", [
+        manager,
+      ])
+    ).rows[0].assigned_users;
     if (!origin || origin.length <= 0) {
-      const ret = await db.query('UPDATE admin_user SET assigned_users = $1 where id = $2 RETURNING *', [`${userId}`, manager]);
+      const ret = await db.query(
+        "UPDATE admin_user SET assigned_users = $1 where id = $2 RETURNING *",
+        [`${userId}`, manager]
+      );
       return res.status(StatusCodes.OK).json(ret.rows[0]);
     } else {
-      const ids = origin.split(',');
+      const ids = origin.split(",");
       if (ids.includes(userId.toString())) {
-        return res.status(StatusCodes.BAD_REQUEST).json('That user is already assigned');
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json("That user is already assigned");
       } else {
-        const ret = await db.query('UPDATE admin_user SET assigned_users = $1 where id = $2 RETURNING *', [`${origin},${userId}`, manager]);
+        const ret = await db.query(
+          "UPDATE admin_user SET assigned_users = $1 where id = $2 RETURNING *",
+          [`${origin},${userId}`, manager]
+        );
         return res.status(StatusCodes.OK).json(ret.rows[0]);
       }
     }
   } catch (error: any) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
   }
 };
 
-const unassignAccountManager: RequestHandler = async (req: Request, res: Response) => {
+const unassignAccountManager: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const { userId, manager } = req.body;
-    console.log('id:', userId, manager);
+    console.log("id:", userId, manager);
 
-    const origin = (await db.query('SELECT assigned_users from admin_user where id = $1', [manager])).rows[0].assigned_users;
-    let ids = origin.split(',');
+    const origin = (
+      await db.query("SELECT assigned_users from admin_user where id = $1", [
+        manager,
+      ])
+    ).rows[0].assigned_users;
+    let ids = origin.split(",");
 
     ids = ids.filter((item: any) => item !== userId.toString());
-    console.log('ids:', ids);
-    await db.query('UPDATE admin_user SET assigned_users = $1 where id = $2', [ids.join(','), manager]);
-    return res.status(StatusCodes.OK).json('unassigned');
+    console.log("ids:", ids);
+    await db.query("UPDATE admin_user SET assigned_users = $1 where id = $2", [
+      ids.join(","),
+      manager,
+    ]);
+    return res.status(StatusCodes.OK).json("unassigned");
   } catch (error: any) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
   }
 };
 
 const getNormalUsers: RequestHandler = async (req: Request, res: Response) => {
-  console.log('get normal user:');
+  console.log("get normal user:");
   try {
     const { accountManager } = req.query;
     let users: any = undefined;
     if (accountManager) {
-      const assignedUsers = await db.query('SELECT assigned_users FROM admin_user WHERE id = $1', [accountManager]);
+      const assignedUsers = await db.query(
+        "SELECT assigned_users FROM admin_user WHERE id = $1",
+        [accountManager]
+      );
       if (assignedUsers.rows[0].assigned_users) {
-        const arr = assignedUsers.rows[0].assigned_users.split(',');
-        users = await db.query('SELECT * from user_list WHERE id = ANY($1)', [arr]);
+        const arr = assignedUsers.rows[0].assigned_users.split(",");
+        users = await db.query("SELECT * from user_list WHERE id = ANY($1)", [
+          arr,
+        ]);
       } else {
         users = {
-          rows: []
+          rows: [],
         };
       }
-
     } else {
-      users = await db.query('SELECT * from user_list');
+      users = await db.query("SELECT * from user_list");
     }
 
     return res.status(StatusCodes.OK).json(users.rows);
   } catch (error: any) {
-    console.log('error getting normal user:', error.message);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    console.log("error getting normal user:", error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
   }
 };
 
 const updateAssigners: RequestHandler = async (req: Request, res: Response) => {
   try {
     const { data } = req.body;
-    console.log('data:', data);
+    console.log("data:", data);
 
     for (const item of data) {
-      await db.query('UPDATE admin_user SET assigned_users = $1 where id = $2', [item.assigned_users, item.id]);
+      await db.query(
+        "UPDATE admin_user SET assigned_users = $1 where id = $2",
+        [item.assigned_users, item.id]
+      );
     }
 
-    return res.status(StatusCodes.OK).json('updated!');
+    return res.status(StatusCodes.OK).json("updated!");
   } catch (error: any) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
   }
 };
 
