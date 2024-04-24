8cc13a89544e1d70c49f79ffd7de6c87e220cd72
Sahil Gupta <sahilhgupta562@gmail.com>
2024-04-25 01:45:32 +0530

    Creator portal reporting dashboard

--- src/routes/creatorRoute.ts
+++ src/routes/creatorRoute.ts
@@ -1,12 +1,15 @@
-import express, { Router } from 'express';
+import express, { Router } from "express";
 
-import auth from '../controller/creator/authController';
-import data from '../controller/creator/dataController';
+import auth from "../controller/creator/authController";
+import data from "../controller/creator/dataController";
 
 const router: Router = express.Router();
 
-router.post('/auth/login', auth.login);
-router.post('/auth/signup', auth.signup);
-router.post('/updatePreferences', data.updateCreatorPreferences);
+router.post("/auth/login", auth.login);
+router.post("/auth/signup", auth.signup);
+router.post("/updatePreferences", data.updateCreatorPreferences);
+router.get("/getCampaign", data.getCampaign);
+router.get("/getNewsletter", data.getNewsletter);
+router.get("/getCampaignList", data.getCampaignList);
 
 export default router;
